0db4d4aae6fb60bcd6d008af55628e00be3ca985
benkivuva <benkivuvak5@gmail.com>
2023-08-29 06:22:52 -0700

    puppet manifest updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,42 +1,8 @@
-# 7-puppet_install_nginx_web_server.pp
-# Install Nginx package
-package { 'nginx':
-  ensure => 'installed',
-}
-
-# Configure Nginx to listen on port 80
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location / {
-        echo \"Hello World!\";
-    }
-
-    location /redirect_me {
-        rewrite ^ http://www.github.com permanent;
-    }
-}
-",
-  require => Package['nginx'],
-}
-
-# Enable the default site by creating a symbolic link
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-  subscribe => [File['/etc/nginx/sites-available/default'], File['/etc/nginx/sites-enabled/default']],
-}
+# install nginx
+exec {'/usr/bin/env apt-get -y update': }
+exec {'/usr/bin/env apt-get -y install nginx': }
+exec {'/usr/bin/env echo "Hello World" > /var/www/html/index.html': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.github.com permanent;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
+exec {'/usr/bin/env service nginx start': }
